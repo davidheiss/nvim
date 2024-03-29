4ecec5dc248383900bd4cf2806eaf9ddb0b712cd
David <david.heiss0406@gmail.com>
2024-03-29 02:29:11 +0100

    new

--- lua/adapters.lua
+++
@@ -1,7 +0,0 @@
-local dap = require("dap")
-
-dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = '/home/dh/.vscode/extensions/ms-vscode.cpptools-1.19.4-linux-x64/debugAdapters/bin/OpenDebugAD7',
-}
